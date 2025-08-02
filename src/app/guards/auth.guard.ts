90717952725cba430bbcf9982af4610f19e1d5d6
Silverplata <jplata@utedevas.es>
2025-08-03 00:48:14 +0200

    Funcionalidad ok

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { toast } from 'ngx-sonner';
 
 export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
-  const token = localStorage.getItem('access_token'); // Cambiado de 'token' a 'access_token'
+  const token = localStorage.getItem('access_token');
   if (token) {
     return true;
   }
+  toast.error('Por favor, inicia sesión para acceder.');
   router.navigate(['/login']);
   return false;
 };
